c58dae120006ff94b62a607fdeea2ee0a44514eb
bonadawg <jbonadon@nd.edu>
2018-10-26 14:25:50 -0400

    Base code

---
+++ pokemon_database.py
@@ -0,0 +1,10 @@
+import sys
+import os
+import requests
+
+class pokemon_database:
+    def __init__(self):
+        self.URL = 'https://www.kaggle.com/shikhar1/pokemon#pokemon.csv'
+
+if __name__ == '__main__':
+    
